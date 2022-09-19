a534117691414c9e69f36218da73ac88c560de3a
Новиков Максим <maxom.nowik@yandex.ru>
2022-09-19 19:44:26 +0300

    Hide secret_key

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.getenv('SECRET_KEY', 'the-best-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
